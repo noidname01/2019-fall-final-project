b76c165fb3a3ce069043cf85855b9cd9d1c0ba3b
noidname01 <tim983649@gmail.com>
2019-12-12 23:48:57 +0800

    add random loop play

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Music_Player:
     def __init__(self):
         self.cur_path = os.getcwd()+'\\downloads'
         self.filelist = []
+        self.playlist = []
         self.ispause = False
         self.loop_play_times = 0
         self.isloop_play = False
@@ -34,68 +35,68 @@ class Music_Player:
         for file_index in range(len(self.filelist)):
             self.filelist[file_index] = 'downloads\\'+ self.filelist[file_index]
             
-        self.original_filelist = self.filelist+[]
+        self.playlist = self.filelist+[]
         #print(self.filelist)
         
         self.pause_text = StringVar()
         self.pause_text.set("play" if self.ispause else "pause")
         self.loop_text = StringVar()
+        self.loop_text.set("single play" if self.isloop_play else "loop play")
         self.random_text = StringVar()
+        self.random_text.set("order play" if self.israndom_play else "random play")
+        self.label_text = StringVar()
         
-        button1 = Button(frame1, textvariable =self.pause_text , width = 20 , command = self.pause)
+        
+        button1 = Button(frame1, textvariable = self.pause_text , width = 20 , command = self.pause)
         button1.grid(row = 0, column = 0, padx = 5, pady = 5)
-        button2 = Button(frame1, textvariable = "single play" if self.isloop_play else "loop play", width = 20, command = self.loop_play)
+        button2 = Button(frame1, textvariable = self.loop_text , width = 20, command = self.loop_play)
         button2.grid(row = 0, column = 1, padx = 5, pady = 5)
-        button3 = Button(frame1, textvariable = "order play" if self.israndom_play else "random play", width = 20, command = self.random_play)
+        button3 = Button(frame1, textvariable = self.random_text , width = 20, command = self.random_play)
         button3.grid(row = 0, column = 2, padx = 5, pady = 5)
         button4 = Button(frame1, text = "next song", width = 20, command = self.next_song)
         button4.grid(row = 0, column = 3, padx = 5, pady = 5)
         
-        mixer.music.load(self.filelist[self.count])
+        frame2 = Frame(self.window)
+        frame2.pack()
+        label = Label(frame2,textvariable = self.label_text)
+        label.pack()
+        
+        mixer.music.load(self.playlist[self.count])
         mixer.music.play(loops = 0)
+        self.nowplaying = self.playlist[self.count]
+        self.label_text.set(self.nowplaying)
+        
+        
         
         self.window.protocol("WM_DELETE_WINDOW",self.stop)
         self.window.mainloop()
         
         
-        
-        
+                
+                
     def counter(self):
-        if self.count == len(self.filelist)-1:
+        if self.count == len(self.playlist)-1 or self.isloop_play:
             self.count = 0
         else:
             self.count += 1
     
-    def play(self):
-        
-        if not mixer.music.get_busy() and not self.ispause:
-            
-            self.next_song()
-            
-        
-        """if self.is_next_song:
-            self.is_next_song = False
-            
-            self.play()
-            self.counter()
-        else:
-            self.counter()"""
-            
-        
-        """if self.isloop_play and not mixer.music.get_busy():
-            mixer.music.play(loops = -1)
-        else:
-            self.counter()"""
     def random_play(self):
-        if self.israndom_play:
+        if not self.israndom_play:
             
-            random.shuffle(self.filelist)
+            random.shuffle(self.playlist)
+            self.label_text.set(self.nowplaying)
+            self.israndom_play = True
             
         else:
-            self.filelist = self.original_filelist
-            self.count = self.original_filelist.index(self.nowplaying)
+            self.playlist = self.filelist
+            self.count = self.playlist.index(self.nowplaying)
+            self.israndom_play = False
+        
+        self.random_text.set("order play" if self.israndom_play else "random play")
         
     def pause(self):
+        if self.is_next_song:
+            self.is_next_song = False
         
         if not self.ispause:
             mixer.music.pause()
@@ -105,33 +106,28 @@ class Music_Player:
             self.ispause = False
             
         self.pause_text.set("play" if self.ispause else "pause")
-    def loop_play(self):
-        if self.isloop_play:
-            
-            if not mixer.music.get_busy() and not self.ispause:
-                mixer.music.load(self.filelist[self.count])
-                mixer.music.play(loops = -1)
-                
-            else:
-                while not self.is_next_song:
-                    if not mixer.music.get_busy() and not self.ispause:
-                        mixer.music.load(self.filelist[self.count])
-                        mixer.music.play(loops = -1)
-                        break
-                
-        else:
-            while not self.is_next_song:
-                if not mixer.music.get_busy() and not self.ispause:
-                        self.next_song()
-                        break
+        
         
     def next_song(self):
+        self.ispause = False
+        self.pause_text.set("play" if self.ispause else "pause")
         mixer.music.stop()
         self.counter()
-        mixer.music.load(self.filelist[self.count])
+        mixer.music.load(self.playlist[self.count])
         mixer.music.play(loops = 0)
-        self.nowplaying = self.filelist[self.count]
-        print(self.nowplaying)
+        self.nowplaying = self.playlist[self.count]
+        self.label_text.set(self.nowplaying)
+        
+    def loop_play(self):
+        if self.isloop_play:
+            self.playlist = self.filelist
+            self.count = self.playlist.index(self.nowplaying)
+            self.isloop_play = False
+        else:
+            self.playlist = [self.nowplaying]
+            self.isloop_play = True
+        
+        self.loop_text.set("single play" if self.isloop_play else "loop play")
         
     def set_volume(self,volume):
         mixer.music.set_volume(volume/100)
