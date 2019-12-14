7fb4e0295d87c4112d9f410e0ddcc1ede72f7c1d
noidname01 <tim983649@gmail.com>
2019-12-14 14:28:21 +0800

    autoplay player

--- player.py
+++ player.py
@@ -2,6 +2,9 @@ from pygame import mixer
 from tkinter import *
 import os
 import random
+import threading
+import sys
+from time import sleep
 
 class Music_Player:
     def __init__(self):
@@ -13,7 +16,7 @@ class Music_Player:
         self.isloop_play = False
         self.count = 0
         self.is_next_song = False
-        self.volume = 0.3
+        
         self.israndom_play = False
         self.nowplaying = str()
         
@@ -47,6 +50,10 @@ class Music_Player:
         self.label_text = StringVar()
         
         
+        self.volume = 30
+        
+        
+        
         button1 = Button(frame1, textvariable = self.pause_text , width = 20 , command = self.pause)
         button1.grid(row = 0, column = 0, padx = 5, pady = 5)
         button2 = Button(frame1, textvariable = self.loop_text , width = 20, command = self.loop_play)
@@ -55,24 +62,44 @@ class Music_Player:
         button3.grid(row = 0, column = 2, padx = 5, pady = 5)
         button4 = Button(frame1, text = "next song", width = 20, command = self.next_song)
         button4.grid(row = 0, column = 3, padx = 5, pady = 5)
+        button5 = Button(frame1, text = "previous song", width = 20, command = self.previous_song)
+        button5.grid(row = 0, column = 4, padx = 5, pady = 5)
         
         frame2 = Frame(self.window)
         frame2.pack()
         label = Label(frame2,textvariable = self.label_text)
         label.pack()
         
+        frame3 = Frame(self.window)
+        frame3.pack()
+        valueBar = Scale(frame3,command = self.set_volume, from_= 0 , to = 100 , orient = "horizontal")
+        valueBar.set(self.volume)
+        valueBar.pack()
+        
         mixer.music.load(self.playlist[self.count])
+        mixer.music.set_volume(self.volume/100)
         mixer.music.play(loops = 0)
         self.nowplaying = self.playlist[self.count]
-        self.label_text.set(self.nowplaying)
+        self.label_text.set(self.nowplaying.replace("downloads\\",""))
         
-        
+        t = threading.Thread(target = self.play)
+        t.setDaemon(True)
+        t.start()
         
         self.window.protocol("WM_DELETE_WINDOW",self.stop)
         self.window.mainloop()
         
-        
-                
+    
+            
+    def decounter(self):
+        if self.count == 0 and not self.isloop_play:
+            self.count = len(self.playlist)-1
+            
+        elif self.isloop_play:
+            self.count = 0
+            
+        else:
+            self.count -= 1
                 
     def counter(self):
         if self.count == len(self.playlist)-1 or self.isloop_play:
@@ -116,7 +143,17 @@ class Music_Player:
         mixer.music.load(self.playlist[self.count])
         mixer.music.play(loops = 0)
         self.nowplaying = self.playlist[self.count]
-        self.label_text.set(self.nowplaying)
+        self.label_text.set(self.nowplaying.replace("downloads\\",""))
+        
+    def previous_song(self):
+        self.ispause = False
+        self.pause_text.set("play" if self.ispause else "pause")
+        mixer.music.stop()
+        self.decounter()
+        mixer.music.load(self.playlist[self.count])
+        mixer.music.play(loops = 0)
+        self.nowplaying = self.playlist[self.count]
+        self.label_text.set(self.nowplaying.replace("downloads\\",""))
         
     def loop_play(self):
         if self.isloop_play:
@@ -130,11 +167,17 @@ class Music_Player:
         self.loop_text.set("single play" if self.isloop_play else "loop play")
         
     def set_volume(self,volume):
-        mixer.music.set_volume(volume/100)
+        mixer.music.set_volume(int(volume)/100)
         
     def stop(self):
         mixer.music.stop()
         self.window.destroy()
+        sys.exit()
         
-
+    def play(self):
+        while True:
+            if not mixer.music.get_busy() and not self.ispause:
+                self.next_song()
+            sleep(0.25)
+        
 Music_Player()
